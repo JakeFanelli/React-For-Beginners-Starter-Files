2d530f471725688901d00c902407987a91419adf
Jake <jxf6552@rit.edu>
2019-01-15 15:55:08 -0500

    using state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,16 +3,26 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 
-
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({
+      fishes
+    });
+  };
+  render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-        <Header />
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-class Header extends React.Component{
-  render(){
+class Header extends React.Component {
+  render() {
     return (
       <header className="top">
-        <h1>Catch 
+        <h1>
+          Catch
           <span className="ofThe">
-          <span className="of">Of</span>
-          <span className="the">The</span>
+            <span className="of">Of</span>
+            <span className="the">The</span>
           </span>
-          Day</h1>
+          Day
+        </h1>
         <h3 className="tagline">
-          <span>Fresh Daily</span>
+          <span>{this.props.tagline}</span>
         </h3>
       </header>
     );
