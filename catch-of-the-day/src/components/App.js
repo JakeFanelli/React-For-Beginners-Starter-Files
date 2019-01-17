da68de390a76989db474f3002fc5177cf9ad88c2
Jake <jxf6552@rit.edu>
2019-01-16 19:28:50 -0500

    Using Order State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -20,6 +20,16 @@ class App extends React.Component {
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
+
+  addToOrder = key => {
+    //take a copy of state
+    const order = { ...this.state.order };
+    //either add or update number in ourder
+    order[key] = order[key] + 1 || 1;
+    //call setstate to update our state obj
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -27,7 +37,12 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
