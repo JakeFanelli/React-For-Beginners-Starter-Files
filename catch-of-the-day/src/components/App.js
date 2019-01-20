39f32b5f7adf2274956da24a2bddffd00502ff1a
Jake <jxf6552@rit.edu>
2019-01-20 16:59:36 -0500

    Revert "lifecycle methods, firebase, updating Order"
    This reverts commit 278b2042834056a3ca86a1f014f75fe8af6d7b85.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,26 +4,12 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
-import base from "../base";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
-
-  componentDidMount() {
-    const { params } = this.props.match;
-    this.ref = base.syncState(`${params.storeId}/fishes`, {
-      context: this,
-      state: "fishes"
-    });
-  }
-
-  componentWillUnmount() {
-    base.removeBinding(this.ref);
-  }
-
   addFish = fish => {
     const fishes = { ...this.state.fishes };
     fishes[`fish${Date.now()}`] = fish;
@@ -31,7 +17,6 @@ class App extends React.Component {
       fishes
     });
   };
-
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -61,7 +46,7 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
